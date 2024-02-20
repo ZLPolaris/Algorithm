64f45504fca41cc6497922115f4dd2fdcab50007
zhuxiaolong <zhuxiaolong@4paradigm.com>
2024-02-20 18:24:39 +0800

    1834

--- leet-code-java/src/main/java/com/learn/LC1834/SolutionOne.java
+++ leet-code-java/src/main/java/com/learn/LC1834/SolutionOne.java
@@ -1,5 +1,7 @@
 package com.learn.LC1834;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class SolutionOne {
@@ -9,21 +11,32 @@ public class SolutionOne {
         int finishedTime = 1;
         int k = 0;
         int[] res = new int[tasksLength];
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> (tasks[o1][1] - tasks[o1][0]) - (tasks[o2][1] - tasks[o2][0]));
+        int[][] tasksData = new int[tasksLength][3];
+        for (int i = 0; i < tasksLength; i++) {
+            tasksData[i] = new int[]{tasks[i][0], tasks[i][1], i};
+        }
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> tasks[o1][1] - tasks[o2][1]);
+        Arrays.sort(tasksData, (o1, o2) -> o1[0] - o2[0]);
         int enqueueIndex = 0;
         while (k < tasksLength) {
-            for (; enqueueIndex < tasksLength && tasks[enqueueIndex][0] >= time; enqueueIndex++) {
+            for (; enqueueIndex < tasksLength && tasksData[enqueueIndex][0] == time; enqueueIndex++) {
                 priorityQueue.add(enqueueIndex);
             }
             if (time >= finishedTime) {
                 if (!priorityQueue.isEmpty()) {
                     int taskId = priorityQueue.poll();
-                    finishedTime = time + tasks[taskId][1] - tasks[taskId][0];
-                    res[k++] = taskId;
+                    finishedTime = time + tasksData[taskId][1];
+                    res[k++] = tasksData[taskId][2];
                 }
             }
             time++;
         }
         return res;
     }
+
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(new SolutionOne().getOrder(new int[][]{
+//                {1, 2}, {2, 4}, {3, 2}, {4, 1},
+//        })));
+//    }
 }
